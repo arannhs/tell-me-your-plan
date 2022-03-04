d5464f60fb8e32e2b4444056a39756bee91eb081
arannhs <arannhs@gmail.com>
2022-03-04 22:48:51 +0900

    JS: greeting

--- js/greeting.js
+++ js/greeting.js
@@ -1,8 +1,20 @@
+const greetingContainer = document.getElementById("greeting");
 const loginForm = document.getElementById("login-form");
+const loginInput = document.getElementById("login-input");
 
 function onLogin(event){
     event.preventDefault();
+
+    const userName = loginInput.value;
+    localStorage.setItem("userName", userName);
     
+    loginForm.classList.add("hidden");
+    
+    const greetingUser = document.createElement("h2");
+    greetingUser.innerText = `Hello ${userName}!`;
+
+    greetingContainer.append(greetingUser);
+
 }
 
 loginForm.addEventListener("submit", onLogin);

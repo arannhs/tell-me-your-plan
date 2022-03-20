d1e10cd3d9699fc89f2afc808bc3a849fcdeba94
arannhs <arannhs@gmail.com>
2022-03-20 18:56:32 +0900

    html: background

--- src/background.js
+++ src/background.js
@@ -1,3 +1,3 @@
 const imgNum = Math.floor(Math.random() * 8) + 1;
 
-document.body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url('/image/${imgNum}.jpg')`;
+document.body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url('image/${imgNum}.jpg')`;
